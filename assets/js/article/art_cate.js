013f739b6f1a1ca29f225cf5c41199c46b85cb7c
disanyanshijie.gw <908592508@qq.com>
2023-03-24 14:04:04 +0800

    完成文章类别的功能开发

---
+++ assets/js/article/art_cate.js
@@ -0,0 +1,53 @@
+// alert('111')
+$(function () {
+    var layer = layui.layer
+    var form = layui.form
+    initArtCateList()
+
+    // 获取文章分类的列表
+    function initArtCateList() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                // 渲染模板
+                var htmlStr = template('tpl-table', res)
+                $('tbody').html(htmlStr)
+            }
+        })
+    }
+
+    // 为添加类别按钮绑定点击事件
+    var indexAdd = null
+    $('#btnAddCate').on('click', function () {
+        indexAdd = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '添加文章分类',
+            content: $('#dialog-add').html()
+        })
+    })
+
+    // 通过代理的形式，为 form-add 表单绑定 submit 事件
+    // 因为一开始页面加载的时候是没有form-add表单的，
+    // 这个表单是动态添加上去的，即当用户点击添加分类之后才有的
+    $('body').on('submit', '#form-add', function (e) {
+        console.log('ok')
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/addcates',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('新增分类失败！')
+                }
+                initArtCateList()
+                layer.msg('新增分类成功！')
+                // 根据索引，关闭对应的弹出层
+                layer.close(indexAdd)
+            }
+        })
+    })
+
+})
